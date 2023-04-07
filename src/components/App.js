0c67842c922eaff20a07b160f4fc7c1e9224433c
James Parrish <jthomasparrishjr@gmail.com>
2023-04-06 22:42:58 -0500

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,16 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Home username={user.name} city={user.city} color={user.color} />
+        <About bio={user.bio} links={user.links} />
+      </div>
+    );
+  }
 }
 
 export default App;
